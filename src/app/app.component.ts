ba5ecefca442b8e18540c9bbb0f727194a350d4d
Dcode22 <dovidylevine@gmail.com>
2023-02-18 20:49:00 +0200

    added fetch request testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'high-wire-frontend';
+export class AppComponent implements OnInit {
+  userAction = async () => {
+    const response = await fetch('https://high-wire-news.herokuapp.com/articles');
+    const myJson = await response.json(); //extract JSON from the http response
+    // do something with myJson
+    console.log(myJson)
+  }
+  ngOnInit(): void {
+    console.log('working')
+    this.userAction()
+  }
 }
