0d6b1bc075a75af9e19786c4dd194a44973e1725
Dovid Levine <dovidlevine@Dovids-MacBook-Pro.local>
2024-06-10 11:26:57 +0300

    updated api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiEndpoint: 'https://high-wire-backend.vercel.app' // get actual api endpoint when deploying
+    apiEndpoint: 'https://high-wire-backend-pmkuf8awi-daves-projects-5725bc28.vercel.app' // get actual api endpoint when deploying
 };
